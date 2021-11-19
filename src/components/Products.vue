8e052ba24f809278133df34548440a6c2d2f5f0a
SiriusStar42 <sirius.star4242@gmail.com>
2021-11-19 16:53:26 +0300

    created project

---
+++ src/components/Products.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row product-container">
+    <app-product v-for="product in productsList" :key="product.index">
+      <img
+        class="card-img-top"
+        :src="product.selectedImage" 
+        :alt="product.title"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{product.title}}</h5>
+        <small> <strong>Adet : </strong>{{product.count}}</small>
+        <br />
+        <small> <strong>Fiyat : </strong>{{product.price}}</small>
+        <br />
+        <small> <strong>Tutar : </strong>{{product.totalPrice}}</small>
+      </div>
+    </app-product>
+  </div>
+</template>
+<script>
+import Product from "./Product.vue";
+import {eventBus} from "../main";
+
+export default {
+  components: {
+    appProduct: Product,
+  },
+  data() {
+      return {
+          productsList: [],
+      }
+  },
+  created() {
+    eventBus.$on("productAdded", (product) => {
+      this.productsList.push(product)
+    });
+  }
+};
+</script>
